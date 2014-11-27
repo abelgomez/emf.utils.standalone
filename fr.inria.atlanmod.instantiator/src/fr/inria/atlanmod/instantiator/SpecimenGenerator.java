b0e37c1ed66e940126e53d803dd5dfa5c75b3d98
Abel GOMEZ LLANA <abel.gomez-llana@mines-nantes.fr>
2014-11-27 13:10:43 +0100

    Changed options

--- fr.inria.atlanmod.instantiator/src/fr/inria/atlanmod/instantiator/SpecimenGenerator.java
+++ fr.inria.atlanmod.instantiator/src/fr/inria/atlanmod/instantiator/SpecimenGenerator.java
@@ -17,18 +17,19 @@ import static com.google.common.primitives.Primitives.isWrapperType;
 import static com.google.common.primitives.Primitives.unwrap;
 
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
-import org.apache.commons.cli.PosixParser;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EAttribute;
@@ -52,15 +53,31 @@ import fr.inria.atlanmod.instantiator.util.UniformLongDistribution;
 
 /**
  * @author <a href="mailto:mikael.barbero@obeo.fr">Mikael Barbero</a>
+ * @author agomez
  *
  */
 public class SpecimenGenerator {
 
-	private static final String METAMODEL = "metamodel";
-	private static final String OUT_DIR = "outdir";
-	private static final String MODELS = "nmodels";
-	private static final String SIZE = "size";
-	private static final String SEED = "seed";
+	private static final String METAMODEL 		= "m";
+	private static final String METAMODEL_LONG 	= "metamodel";
+	private static final String OUTPUT_DIR 		= "o";
+	private static final String OUTPUT_DIR_LONG	= "output-dir";
+	private static final String N_MODELS		= "n";
+	private static final String N_MODELS_LONG	= "number-models";
+	private static final String SIZE 			= "s";
+	private static final String SIZE_LONG		= "size";
+	private static final String SEED 			= "e";
+	private static final String SEED_LONG 		= "seed";
+
+
+	private static class OptionComarator<T extends Option> implements Comparator<T> {
+	    private static final String OPTS_ORDER = "monse";
+
+	    @Override
+		public int compare(T o1, T o2) {
+	        return OPTS_ORDER.indexOf(o1.getOpt()) - OPTS_ORDER.indexOf(o2.getOpt());
+	    }
+	}
 
 	protected final Random generator;
 	protected final ISpecimenConfiguration c;
@@ -76,33 +93,43 @@ public class SpecimenGenerator {
 
 		configureOptions(options);
 
-		CommandLineParser parser = new PosixParser();
+		CommandLineParser parser = new GnuParser();
 
 		try {
 			CommandLine commandLine = parser.parse(options, args);
 
 			String metamodel = commandLine.getOptionValue(METAMODEL);
 			DefaultModelGenerator modelGen = new DefaultModelGenerator(URI.createFileURI(metamodel));
-			if (commandLine.hasOption(OUT_DIR)) {
-				String outDir = commandLine.getOptionValue(OUT_DIR);
+			if (commandLine.hasOption(OUTPUT_DIR)) {
+				String outDir = commandLine.getOptionValue(OUTPUT_DIR);
 				modelGen.setSamplesPath(Paths.get(outDir));
+			} else {
+				modelGen.setSamplesPath(Paths.get("."));
 			}
-			if (commandLine.hasOption(MODELS)) {
-				int models = ((Number) commandLine.getParsedOptionValue(MODELS)).intValue();
+			if (commandLine.hasOption(N_MODELS)) {
+				int models = ((Number) commandLine.getParsedOptionValue(N_MODELS)).intValue();
 				modelGen.setSetSize(new int[] { models });
+			} else {
+				modelGen.setSetSize(new int[] { 1 });
 			}
 			if (commandLine.hasOption(SIZE)) {
 				long size = ((Number) commandLine.getParsedOptionValue(SIZE)).longValue();
 				modelGen.setModelsSize(new long[] { size });
+			} else {
+				modelGen.setModelsSize(new long[] { 1000 });
 			}
 			if (commandLine.hasOption(SEED)) {
 				long seed = ((Number) commandLine.getParsedOptionValue(SEED)).longValue();
 				modelGen.setSeed(seed);
+			} else {
+				modelGen.setSeed(System.currentTimeMillis());
 			}
 			modelGen.runGeneration();
 		} catch (ParseException e) {
 			System.err.println(e.getLocalizedMessage());
 			HelpFormatter formatter = new HelpFormatter();
+			formatter.setOptionComparator(new OptionComarator<Option>());
+			formatter.setWidth(80);
 			formatter.printHelp("java -jar <this-file.jar>", options, true);
 		}
 	}
@@ -113,40 +140,45 @@ public class SpecimenGenerator {
 	 * @param options
 	 */
 	private static void configureOptions(Options options) {
-		Option sourcemmOpt = OptionBuilder.create(METAMODEL);
-		sourcemmOpt.setArgName("metamodel.ecore");
-		sourcemmOpt.setDescription("Ecore metamodel");
-		sourcemmOpt.setArgs(1);
-		sourcemmOpt.setRequired(true);
+		Option metamodelOpt = OptionBuilder.create(METAMODEL);
+		metamodelOpt.setLongOpt(METAMODEL_LONG);
+		metamodelOpt.setArgName("path_to_metamodel.ecore");
+		metamodelOpt.setDescription("Ecore metamodel");
+		metamodelOpt.setArgs(1);
+		metamodelOpt.setRequired(true);
 
-		Option outDirOpt = OptionBuilder.create(OUT_DIR);
-		outDirOpt.setArgName("output.dir");
-		outDirOpt.setDescription("Ecore metamodel");
+		Option outDirOpt = OptionBuilder.create(OUTPUT_DIR);
+		outDirOpt.setLongOpt(OUTPUT_DIR_LONG);
+		outDirOpt.setArgName("path_to_output.dir");
+		outDirOpt.setDescription("Output directory (defaults to working dir)");
 		outDirOpt.setArgs(1);
 
-		Option numModelsOpt = OptionBuilder.create(MODELS);
-		numModelsOpt.setArgName("models");
-		numModelsOpt.setDescription("Number of generated models");
-		numModelsOpt.setType(Number.class);
-		numModelsOpt.setArgs(1);
+		Option nModelsOpt = OptionBuilder.create(N_MODELS);
+		nModelsOpt.setLongOpt(N_MODELS_LONG);
+		nModelsOpt.setArgName("models");
+		nModelsOpt.setDescription("Number of generated models (defaults to 1)");
+		nModelsOpt.setType(Number.class);
+		nModelsOpt.setArgs(1);
 
 		Option sizeOption = OptionBuilder.create(SIZE);
+		sizeOption.setLongOpt(SIZE_LONG);
 		sizeOption.setArgName("size");
-		sizeOption.setDescription("Models' size");
+		sizeOption.setDescription("Models' size (defaults to 1000)");
 		sizeOption.setType(Number.class);
 		sizeOption.setArgs(1);
 
-		Option recordsPerNodeOption = OptionBuilder.create(SEED);
-		recordsPerNodeOption.setArgName("seed");
-		recordsPerNodeOption.setDescription("Seed number");
-		recordsPerNodeOption.setType(Number.class);
-		recordsPerNodeOption.setArgs(1);
+		Option seedOption = OptionBuilder.create(SEED);
+		seedOption.setLongOpt(SEED_LONG);
+		seedOption.setArgName("seed");
+		seedOption.setDescription("Seed number (random by default)");
+		seedOption.setType(Number.class);
+		seedOption.setArgs(1);
 
-		options.addOption(sourcemmOpt);
+		options.addOption(metamodelOpt);
 		options.addOption(outDirOpt);
-		options.addOption(numModelsOpt);
+		options.addOption(nModelsOpt);
 		options.addOption(sizeOption);
-		options.addOption(recordsPerNodeOption);
+		options.addOption(seedOption);
 	}
 
 	public SpecimenGenerator(ISpecimenConfiguration configuration) {
